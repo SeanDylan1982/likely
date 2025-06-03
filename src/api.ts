6d7b7550bb9e048b8b8ab2afff50070ca94eb4fe
Sean Dylan Patterson <seandylanpatterson@gmail.com>
2025-06-03 16:47:09 +0200

    Improve genre filtering for movie carousels

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,5 @@
+import { Movie, TVShow, ContentType, SearchResponse, ContentDetails, Genre } from './types';
+
 const AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YjM1NzA1ZDQ2OWQ1OTdmMWY2YWQyM2Q0MjY0YjMwNCIsIm5iZiI6MTc0ODU5Nzc0MC42MzEsInN1YiI6IjY4Mzk3YmVjNjI1OTg5OTE4MzA4MDkwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zWnHs6I27PyqJ2mvTIDEfCENKj17dbrtEpl3K-RJ2sA';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -53,12 +55,21 @@ export async function getTopRatedContent(type: ContentType): Promise<Movie[] | T
 }
 
 export async function getContentByGenre(type: ContentType, genreId: number): Promise<Movie[] | TVShow[]> {
-  const response = await fetch(
-    `${BASE_URL}/discover/${type}?with_genres=${genreId}&sort_by=popularity.desc`,
-    { headers }
-  );
-  const data: SearchResponse = await response.json();
-  return data.results;
+  // Fetch multiple pages to get more content per genre
+  const pages = await Promise.all([1, 2].map(page => 
+    fetch(
+      `${BASE_URL}/discover/${type}?with_genres=${genreId}&sort_by=vote_count.desc&page=${page}`,
+      { headers }
+    ).then(res => res.json())
+  ));
+
+  // Combine results from all pages
+  const results = pages.flatMap(page => page.results);
+  
+  // Sort by popularity and take top 20
+  return results
+    .sort((a, b) => b.popularity - a.popularity)
+    .slice(0, 20);
 }
 
 export async function getGenres(type: ContentType): Promise<Genre[]> {
